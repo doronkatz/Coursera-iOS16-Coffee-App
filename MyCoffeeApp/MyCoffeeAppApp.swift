e31a4053d5d20e68a78a21a94b62f2421de4d609
Doron Katz <doron@doronkatz.com>
2025-05-13 16:20:18 -0700

    Initial Commit

---
+++ MyCoffeeApp/MyCoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoffeeAppApp.swift
+//  MyCoffeeApp
+//
+//  Created by Doron Katz on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCoffeeAppTests/MyCoffeeAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyCoffeeAppTests.swift
+//  MyCoffeeAppTests
+//
+//  Created by Doron Katz on 5/13/25.
+//
+
+import Testing
+
+struct MyCoffeeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
